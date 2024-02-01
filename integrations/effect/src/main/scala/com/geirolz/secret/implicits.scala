02bf7ab3acb4f2b42fc05eefa2032719e1c0def8
geirolz <david.geirola@gmail.com>
2024-02-01 18:20:12 +0100

    Improvements

--- integrations/effect/src/main/scala/com/geirolz/secret/implicits.scala
+++ integrations/effect/src/main/scala/com/geirolz/secret/implicits.scala
@@ -5,5 +5,5 @@ import cats.effect.{Async, Resource}
 extension [T](secret: Secret[T])
   inline def resource[F[_]: Async]: Resource[F, T] =
     Resource
-      .make(secret.duplicate[F])(s => Async[F].delay(s.destroy()))
+      .fromAutoCloseable(secret.duplicate[F])
       .evalMap(_.accessValue[F])
