acb509cc87820006c2edf0f1ffd086e2af8b3c09
geirolz <david.geirola@gmail.com>
2024-03-12 16:11:30 +0100

    Add SecretDestroyed stacktrace.

--- core/src/main/scala/com/geirolz/secret/SecretDestroyed.scala
+++ core/src/main/scala/com/geirolz/secret/SecretDestroyed.scala
@@ -6,8 +6,7 @@ import com.geirolz.secret.util.Location
 import scala.util.control.NoStackTrace
 
 case class SecretDestroyed(destructionLocation: Location)
-    extends RuntimeException(s"This secret has been already destroyed.\nLocation: $destructionLocation")
-    with NoStackTrace
+    extends RuntimeException(s"This secret has been already destroyed here: $destructionLocation")
 
 object SecretDestroyed:
   given Show[SecretDestroyed] = Show(_.getMessage)
