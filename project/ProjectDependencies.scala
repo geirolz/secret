0b70c5c5a39e7d8740753c13615201730e6d0943
scala-steward <122865766+scala-steward-geirolz[bot]@users.noreply.github.com>
2024-06-16 00:23:36 +0000

    Update pureconfig-core to 0.17.7

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -12,7 +12,7 @@ object ProjectDependencies {
   private val scalacheck         = "1.18.0"
   // integrations
   private val catsEffectVersion     = "3.5.4"
-  private val pureConfigVersion     = "0.17.6"
+  private val pureConfigVersion     = "0.17.7"
   private val typesafeConfigVersion = "1.4.3"
   private val cirisVersion          = "3.6.0"
 
