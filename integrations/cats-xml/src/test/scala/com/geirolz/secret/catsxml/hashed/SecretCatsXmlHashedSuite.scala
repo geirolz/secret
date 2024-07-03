4d82415509151ce056994db50d19a071bd3c9df3
geirolz <david.geirola@gmail.com>
2024-07-03 07:11:27 +0200

    Add cats xml encoders

--- integrations/cats-xml/src/test/scala/com/geirolz/secret/catsxml/hashed/SecretCatsXmlHashedSuite.scala
+++ integrations/cats-xml/src/test/scala/com/geirolz/secret/catsxml/hashed/SecretCatsXmlHashedSuite.scala
@@ -1,11 +1,11 @@
-package com.geirolz.secret.catsxml
+package com.geirolz.secret.catsxml.hashed
 
 import cats.xml.codec.Decoder.Result
-import cats.xml.{Xml, XmlData}
 import cats.xml.syntax.*
+import cats.xml.{Xml, XmlData}
 import com.geirolz.secret.{secretTag, OneShotSecret, Secret}
 
-class SecretCatsXmlSuite extends munit.FunSuite:
+class SecretCatsXmlHashedSuite extends munit.FunSuite:
 
   test("Secret should be decoded from xml") {
     val xml: XmlData.XmlString         = Xml.string("secret_value")
@@ -25,12 +25,12 @@ class SecretCatsXmlSuite extends munit.FunSuite:
     val secret: Secret[String] = Secret("secret_value")
     val result: Xml            = secret.toXml
 
-    assert(result == Xml.string(secretTag))
+    assert(result == Xml.string(secret.hashed))
   }
 
   test("OneShotSecret should be encoded to json") {
     val secret: OneShotSecret[String] = OneShotSecret("secret_value")
     val result: Xml                   = secret.toXml
 
-    assert(result == Xml.string(secretTag))
+    assert(result == Xml.string(secret.hashed))
   }
