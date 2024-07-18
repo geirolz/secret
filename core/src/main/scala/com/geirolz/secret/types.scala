550f3a2d2b87b7e947876cabf0591f65a0d897c5
geirolz <david.geirola@gmail.com>
2024-07-18 17:07:42 +0200

    Drop SPassword

--- core/src/main/scala/com/geirolz/secret/types.scala
+++ core/src/main/scala/com/geirolz/secret/types.scala
@@ -1,30 +1,14 @@
 package com.geirolz.secret
 
-import cats.{MonadError, Show}
-import com.geirolz.secret.internal.SecretApi
+import cats.MonadError
 import com.geirolz.secret.strategy.SecretStrategy
-import com.geirolz.secret.util.{Hasher, Location}
+import com.geirolz.secret.util.Hasher
 
 import java.nio.ByteBuffer
-import scala.util.control.NoStackTrace
 
 type PlainValueBuffer      = ByteBuffer
 type ObfuscatedValueBuffer = ByteBuffer
 type KeyBuffer             = ByteBuffer
-
-// alias
-type SString = Secret[String]
-object SString:
-  type OneShot = OneShotSecret[String]
-  def oneShot(value: String, collectDestructionLocation: Boolean = true)(using
-    strategy: SecretStrategy[String],
-    hasher: Hasher
-  ): SString.OneShot = OneShotSecret(value)
-
-  def apply(value: String, collectDestructionLocation: Boolean = true)(using
-    strategy: SecretStrategy[String],
-    hasher: Hasher
-  ): SString = Secret(value)
 
 // private
 private[secret] type MonadSecretError[F[_]] = MonadError[F, ? >: SecretDestroyed]
