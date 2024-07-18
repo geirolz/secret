50178a41c9a7d54e49f9678093025600bd9ab25a
David Geirola <david.geirola@gmail.com>
2024-07-18 17:08:35 +0200

    Improve syntax (#44)
    * Add tests
    
    * Update docs
    
    * Drop SPassword

--- core/src/main/scala/com/geirolz/secret/types.scala
+++ core/src/main/scala/com/geirolz/secret/types.scala
@@ -1,20 +1,14 @@
 package com.geirolz.secret
 
-import cats.{MonadError, Show}
-import com.geirolz.secret.util.Location
+import cats.MonadError
+import com.geirolz.secret.strategy.SecretStrategy
+import com.geirolz.secret.util.Hasher
 
 import java.nio.ByteBuffer
-import scala.util.control.NoStackTrace
 
 type PlainValueBuffer      = ByteBuffer
 type ObfuscatedValueBuffer = ByteBuffer
 type KeyBuffer             = ByteBuffer
-
-// alias
-type SPassword = Secret[String]
-object SPassword:
-  type OneShot = OneShotSecret[String]
-  def apply(value: String): SPassword = Secret(value)
 
 // private
 private[secret] type MonadSecretError[F[_]] = MonadError[F, ? >: SecretDestroyed]
